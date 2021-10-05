a42d832616ab2c2e474992b05cbe0c036ae563bb
Jason Moldovan <moldovanjason@gmail.com>
2021-10-05 13:39:35 -0400

    more

---
+++ src/Components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PinterestIcon from "@material-ui/icons/Pinterest";
+
+function Header() {
+  return (
+    <Wrapper>
+      <LogoWrapper><PinterestIcon/></LogoWrapper>
+      {/* <HomePageButton></HomePageButton>
+      <FollowingButton></FollowingButton>
+      <SeachWrapper>
+        <SeachBarWrapper></SeachBarWrapper>
+      </SeachWrapper>
+      <IconsWrapper></IconsWrapper> */}
+    </Wrapper>
+  );
+}
+
+export default Header;
+
+const Wrapper = styled.div`
+display:flex;
+align-items:center;
+height:56px;
+padding:12px 4px 4px 16px;
+background-color:white;
+color:black;`
+
